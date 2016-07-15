76e6beb1534d11886279c58355ff25e0bd4990a4
Andre Soares <andre@Mniak-PC.local>
2016-07-15 02:10:02 -0300

    Fixes to .npmignore

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 /// <reference path="typings/index.d.ts" />
 
-export * from './src/modal-content'
-export * from './src/modal.component'
-export * from './src/modal.service'
+export * from './src/modal-content';
+export * from './src/modal-dialog';
+export * from './src/modal-params';
+export * from './src/modal-result';
+export * from './src/modal.component';
+export * from './src/modal.service';
